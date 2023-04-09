e1f47f4392fc34c092473f9ca79f2635e578879d
Philg112 <pgrujovski@protonmail.com>
2023-04-09 11:27:45 +1000

    lord help me

--- pollr.nix
+++ pollr.nix
@@ -1,11 +1,15 @@
-{ mkDerivation, base, lib }:
+{ mkDerivation, aeson, base, containers, lib, mtl, servant
+, servant-server, text, time, warp
+}:
 mkDerivation {
   pname = "pollr";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base ];
-  license = lib.licenses.mit;
+  executableHaskellDepends = [
+    aeson base containers mtl servant servant-server text time warp
+  ];
+  license = "unknown";
   mainProgram = "pollr";
 }
--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
-    buildInputs = [ pkgs.haskell-language-server pkgs.haskell.compiler.ghc943 ]; #pkgs.glibc.static pkgs.zlib.static pkgs.gmp ];
+    buildInputs = [ pkgs.haskell-language-server pkgs.haskell.compiler.ghc902 pkgs.zlib ]; #pkgs.glibc.static pkgs.zlib.static pkgs.gmp ];
 }
