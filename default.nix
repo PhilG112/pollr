33b14472bebaa580287de04b0f3212b2b9b49845
Philg112 <pgrujovski@protonmail.com>
2023-04-14 14:43:03 +1000

    SO CLOSE

--- default.nix
+++ default.nix
@@ -1,24 +1,29 @@
-{ compiler ? "ghc902" }:
+/* { compiler ? "ghc902" }: */
+
+/* let */
+/*     config = { */
+/*         packageOverrides = pkgs: rec { */
+/*             hls = pkgs.haskell-language-server.override { */
+/*                 supportedGhcVersions = ["902" ]; */
+/*             }; */
+/*             haskell = pkgs.haskell // { */
+/*                 packages = pkgs.haskell.packages // { */
+/*                     "${compiler}" = pkgs.haskell.packages."${compiler}".override { */
+/*                         overrides = haskellPackagesNew: haskellPackagesOld: rec { */
+/*                             pollr = haskellPackagesNew.callPackage ./pollr.nix {}; */    
+/*                         }; */
+/*                     }; */
+/*                 }; */
+/*             }; */
+/*         }; */
+/*     }; */
+/*     pkgs = import <nixpkgs> { inherit config; }; */
+/* in */
+/*     { */
+/*         pollr = pkgs.haskell.packages.${compiler}.pollr; */
+/*     } */
 
 let
-    config = {
-        packageOverrides = pkgs: rec {
-            hls = pkgs.haskell-language-server.override {
-                supportedGhcVersions = ["902" ];
-            };
-            haskell = pkgs.haskell // {
-                packages = pkgs.haskell.packages // {
-                    "${compiler}" = pkgs.haskell.packages."${compiler}".override {
-                        overrides = haskellPackagesNew: haskellPackagesOld: rec {
-                            pollr = haskellPackagesNew.callPackage ./pollr.nix {};    
-                        };
-                    };
-                };
-            };
-        };
-    };
-    pkgs = import <nixpkgs> { inherit config; };
+    pkgs = import <nixpkgs> {};
 in
-    {
-        pollr = pkgs.haskell.packages.${compiler}.pollr;
-    }
+    pkgs.haskellPackages.callPackage ./pollr.nix {}
--- shell.nix
+++ shell.nix
@@ -1,13 +1,79 @@
-/* (import ./default.nix).pollr.env */
-let
-    pkgs = import <nixpkgs> {};
-    p = (import ./default.nix).pollr;
-in
-    pkgs.mkShell {
-        buildInputs = [ pkgs.haskell-language-server pkgs.haskell.compiler.ghc902 ]; 
-    } 
+/* let */
+/*     pkgs = import <nixpkgs> {}; */
+/* in */
+/*     pkgs.mkShell { */
+/*         buildInputs = [ pkgs.haskell-language-server pkgs.haskell.compiler.ghc92 ]; */ 
+/*     } */ 
 
 /* { pkgs ? import <nixpkgs> {} }: */
 /* pkgs.mkShell { */
 /*     buildInputs = [ pkgs.haskell-language-server pkgs.haskell.compiler.ghc902 pkgs.zlib ]; #pkgs.glibc.static pkgs.zlib.static pkgs.gmp ]; */
 /* } */
+
+/* with import <nixpkgs> {}; */
+
+/* let */
+/*     pkgs = import <nixpkgs> {}; */
+/*     project = ./pollr.nix; */
+/* in */
+/*     pkgs.mkShell { */
+/*         buildInputs = [ */
+/*             (pkgs.haskell.compiler.ghc902) */
+/*             (pkgs.haskellPackages.callPackage project {}) */
+/*             (pkgs.haskell-language-server) */
+/*         ]; */
+/*    } */
+
+/* with import <nixpkgs> {}; */
+
+/* let */
+/*   pkgs = import <nixpkgs> {}; */
+/*   project = ./pollr.nix; */
+/* in */
+/*   pkgs.haskellPackages.shellFor { */
+/*     # Specify the GHC version here */
+/*     ghc = pkgs.haskell.compiler.ghc902; */
+/*     packages = p: with p; [ */
+/*       # Add any additional dependencies your project requires */
+/*       (pkgs.haskellPackages.callPackage project {}) */
+/*       pkgs.haskell-language-server */
+/*     ]; */
+/*   } */
+
+
+/* with import <nixpkgs> {}; */
+
+/* let */
+/*   project = ./default.nix; */
+/*   pkgs = import <nixpkgs> {}; */
+/* in */
+/*   ghc.withPackages (ps: with ps; [ */
+/*     # Specify the GHC version here */
+/*     (pkgs.haskell.compiler.ghc902.override { */
+/*       override = self: super: { */
+/*         # Add any additional dependencies your project requires */
+/*         haskell-language-server = pkgs.haskell-language-server; */
+/*       }; */
+/*     }) */
+/*     (pkgs.haskellPackages.callPackage project {}) */
+/*   ]) */
+/*   (haskellPackages.buildCabalProject { */
+/*     # Specify the path to your Haskell project */
+/*     pname = "pollr"; */
+/*     path = project; */
+/*   }) */
+
+{pkgs ? import <nixpkgs> {} }:
+
+pkgs.haskellPackages.shellFor {
+    packages = hpkgs: [
+        (hpkgs.callPackage ./pollr.nix { })
+    ];
+
+    nativeBuildInputs = [
+        pkgs.haskell-language-server
+        pkgs.haskell.compiler.ghc92
+        pkgs.libffi
+    ];
+}
+
